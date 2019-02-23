960504b0bfc2fc11d8317198a0ae441c8259d6c6
beepbooprobot <benlangford10@gmail.com>
2019-02-23 23:48:57 +0000

    Initial Commit

---
+++ src/Quaternion/Main.java
@@ -0,0 +1,8 @@
+package Quaternion;
+
+public class Main {
+    public static void main(String[] args) {
+        // This will be a test of using quaternions for 3D graphics.
+        // It will be a demonstration of basic 3D graphics that I could probably do better in Unreal Engine
+    }
+}
---
+++ src/VectorShade/Main.java
@@ -0,0 +1,8 @@
+package VectorShade;
+
+public class Main {
+    public static void main(String[] args) {
+        // This project will display a rotating 3D polygon who's sides will darken/brighten.
+        //It will be a demonstration of vector maths that I could probably do faster in MATLAB but oh well
+    }
+}
